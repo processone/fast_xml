9f90305a28eea4c7ec92cf02f97e1a6532846f81
Mickael Remond <mremond@process-one.net>
2016-01-25 08:34:10 +0100

    Describe more types for Elixir
    We even describe types that are not yet explicit / documented in Erlang API.

--- lib/fast_xml.ex
+++ lib/fast_xml.ex
@@ -5,13 +5,23 @@ defmodule FastXML do
 xml element record, composed of fields: name, attrs, children
 """
   Record.defrecord :xmlel, Record.extract(:xmlel, from_lib: "fast_xml/include/fxml.hrl")
-  
+
   @type xmlel :: record(:xmlel, name: String.t, attrs: [attr], children: [content])
   @type content :: xmlel | cdata
   @type cdata :: {:xmlcdata, String.t}
-  @type attr  :: {String.t, String.t}
+  @type attr  :: {attribute::String.t, value::String.t}
 
-  def start do
-    :application.start(:fast_xml)
-  end
+  Record.defrecord :xmlstreamstart, name: "", attrs: []
+  @type xmlstreamstart :: record(:xmlstreamstart, name: String.t, attrs: [attr])
+
+  Record.defrecord :xmlstreamend, name: ""
+  @type xmlstreamend :: record(:xmlstreamend, name: String.t)  
+
+  Record.defrecord :xmlstreamerror, error: {0, ""}
+  @type xmlstreamerror ::record(:xmlstreamerror, error: error)
+  @type error :: {code::Integer, description::String.t}
+  
+  def start, do: :application.start(:fast_xml)
+  def stop,  do: :application.stop(:fast_xml)
+
 end
