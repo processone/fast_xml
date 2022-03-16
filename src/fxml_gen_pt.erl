bc4a61241b0fb8bdd1914c23bd12995a019aeed8
Badlop <badlop@process-one.net>
2022-03-16 12:42:04 +0100

    Update copyright year to 2022

--- src/fxml_gen_pt.erl
+++ src/fxml_gen_pt.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
 %%% @author Evgeny Khramtsov <ekhramtsov@process-one.net>
-%%% @copyright (C) 2016-2021, Evgeny Khramtsov
+%%% @copyright (C) 2016-2022, Evgeny Khramtsov
 %%% @doc
 %%%
 %%% @end
