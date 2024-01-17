b68517e1269c7212680b693712a697da4d5a708e
Badlop <badlop@process-one.net>
2024-01-17 18:44:55 +0100

    Update copyright year to 2024

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-# Copyright (C) 2002-2022 ProcessOne, SARL. All Rights Reserved.
+# Copyright (C) 2002-2024 ProcessOne, SARL. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
